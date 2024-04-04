e86cea9b7fee56f7c9d9389303a638bb2cef7f19
Samnang Ma <masamnang6@gmail.com>
2024-04-04 19:17:08 +0700

    lessons C++ days 1

---
+++ index.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x = 20;
+    double b = 12;
+
+    double total = --x;
+    double total1 = ++b;
+
+    
+    // int z ;
+    cout<< "This a total 1 "<< total1;
+    cout << "\n this is a total 2 "<< total ;
+    // cout << "\n total" << x + b;
+    return 0;
+}
